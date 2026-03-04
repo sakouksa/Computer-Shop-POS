2ac58ef64843caf008253be736b3923a2ed66808
sakouksa <hjhfg7226@gmail.com>
2026-03-04 19:06:11 +0700

    Update code

--- pos-frontend/src/store/profileStore.js
+++ pos-frontend/src/store/profileStore.js
@@ -1,9 +1,19 @@
 import { create } from "zustand";
-import profile_image from "../assets/img/profile.jpg";
+import { persist, createJSONStorage } from "zustand/middleware";
 
-export const profileStore = create((set) => ({
-  profile: null,
-  setProfile: (params) => set((pre) => ({ profile: params })),
-  logout: (params) => set((pre) => ({ profile: null })),
-}));
+export const profileStore = create(
+  persist(
+    (set) => ({
+      profile: null,
+      access_token: null,
 
+      setProfile: (params) => set((pre) => ({ profile: params })),
+      setAccessToken: (params) => set((pre) => ({ access_token: params })),
+      logout: () => set((pre) => ({ profile: null })),
+    }),
+    {
+      name: "user-profile", // name of the item in the storage (must be unique)
+      storage: createJSONStorage(() => sessionStorage), // (optional) by default, 'localStorage' is used
+    },
+  ),
+);
--- pos-frontend/src/util/request.js
+++ pos-frontend/src/util/request.js
@@ -1,15 +1,10 @@
 import axios from "axios";
 import config from "./config";
-// បង្កើត Object សម្រាប់បកប្រែឈ្មោះ field ទៅជាភាសាខ្មែរ
-const khmerErrorMessages = {
-  name: "សូមបញ្ចូលឈ្មោះតួនាទី!",
-  code: "សូមបញ្ចូលកូដតួនាទី!",
-  description: "សូមបញ្ចូលការពិពណ៌នា!",
-  status: "សូមជ្រើសរើសស្ថានភាព!",
-};
+import { profileStore } from "../store/profileStore";
 
 export const request = (url = "", method = "", data = {}) => {
-  //url = "customers search"
+
+  let access_token = profileStore.getState().access_token;
   return axios({
     url: config.base_url + url,
     method: method, //"get","post" ,"put","delete"
@@ -17,6 +12,7 @@ export const request = (url = "", method = "", data = {}) => {
     headers: {
       Accept: "application/json",
       "Content-Type": "application/json",
+      Authorization : "Bearer" + access_token,
     },
   })
     .then((res) => {
@@ -35,19 +31,25 @@ export const request = (url = "", method = "", data = {}) => {
           errors.message = "មានបញ្ហាបច្ចេកទេសក្នុងប្រព័ន្ធ សូមព្យាយាមម្តងទៀត!";
         }
 
-        if (data.errors) {``
+        if (data.errors) {
            Object.keys(data.errors).map((key) => {
              errors[key] = {
                validateStatus: "error",
-               help: khmerErrorMessages[key] || data.errors[key][0], //get error message
+               help: data.errors[key][0], //get error message
                hasFeedback: true,
              };
            });
         }
         return {
+          error :true,
           status:status,
           errors: errors,
         };
       }
+      // ករណីគ្មាន Internet ឬ Server បិទ
+      return { 
+        error: true,
+        errors: { message: "មិនអាចតភ្ជាប់ទៅកាន់ Server បានទេ!" } 
+      };
     });
 };
